78c10cf8452e18c7056499c611b166d1335066b3
deliriousrhino <ryan@deliriousrhino.com>
2014-03-17 12:34:54 +1100

    Update ExtractZip.m
    Removed background service

--- src/ios/ExtractZip.m
+++ src/ios/ExtractZip.m
@@ -13,26 +13,22 @@
 
 - (void)extract:(CDVInvokedUrlCommand *)command
 {
-   // Run the service in the background
-    [self.commandDelegate runInBackground:^{
-        NSArray* arguments = command.arguments;
-        
-        NSLog(@"Extract zip has been started.");
-        NSString *file = [arguments objectAtIndex:0];
-        NSString *destination = [arguments objectAtIndex:1];
-
-        CDVPluginResult *result;
-        if([SSZipArchive unzipFileAtPath:file toDestination:destination delegate:nil]) {
-            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[destination stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-            [self writeJavascript:[result toSuccessCallbackString:command.callbackId]];
-            NSLog(@"Success!");
-        } else {
-            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Could not extract archive" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-            [self writeJavascript:[result toErrorCallbackString:command.callbackId]];
-            NSLog(@"Error!");
-        }
-
-    }];
+    NSArray* arguments = command.arguments;
+    
+    NSLog(@"Extract zip has been started.");
+    NSString *file = [arguments objectAtIndex:0];
+    NSString *destination = [arguments objectAtIndex:1];
+    
+    CDVPluginResult *result;
+    if([SSZipArchive unzipFileAtPath:file toDestination:destination delegate:nil]) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[destination stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        [self writeJavascript:[result toSuccessCallbackString:command.callbackId]];
+        NSLog(@"Success!");
+    } else {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Could not extract archive" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        [self writeJavascript:[result toErrorCallbackString:command.callbackId]];
+        NSLog(@"Error!");
+    }
 }
 
 - (void)getTempDir:(CDVInvokedUrlCommand *)command
@@ -45,13 +41,14 @@
     NSString* dirPath = [path stringByAppendingPathComponent:dirName];
     
     NSLog(@"Trying to create directory: %@",dirName);
-    bool isCreated = ![[NSFileManager defaultManager] createDirectoryAtPath:dirPath withIntermediateDirectories:NO attributes:nil error: &error];
+    bool isCreated = [[NSFileManager defaultManager] createDirectoryAtPath:dirPath withIntermediateDirectories:YES attributes:nil error: &error];
     
     if (isCreated) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:dirPath];
         NSLog(@"Success!");
     }else{
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Can't create the directory."];
+        NSLog(@"Error!");
     }
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
